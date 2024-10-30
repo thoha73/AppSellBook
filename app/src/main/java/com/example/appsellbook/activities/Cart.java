6ff83c4b7268544cb8e27ced72065480b04e1ae1
ASUS <tranthanhvyvyvy@gmail.com>
2024-10-30 22:25:08 +0700

    thanhvy

--- app/src/main/java/com/example/appsellbook/activities/Cart.java
+++ app/src/main/java/com/example/appsellbook/activities/Cart.java
@@ -44,37 +44,42 @@ public class Cart extends AppCompatActivity {
         cartList.add(new Carts(R.drawable.img_sucmanhtiemthuc, "Sức mạnh tiềm thức (Tái bản năm 2021)", "100.000 đ"));
         CartsAdapter adapter = new CartsAdapter(this, R.layout.cart_item, cartList);
         listView.setAdapter(adapter);
-
+        Button btnOrder = findViewById(R.id.btn_order);
+        btnOrder.setOnClickListener(v -> {
+            Intent intent = new Intent(Cart.this,OrderTotal.class);
+            startActivity(intent);
+            overridePendingTransition(0, 0);
+        });
         BottomNavigationView bottom_NavigationView;
         bottom_NavigationView = findViewById(R.id.bottom_navigation);
         bottom_NavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
-                if(id==R.id.menu_home){
+                if (id == R.id.menu_home) {
                     startActivity(new Intent(getApplicationContext(), Home.class));
-                    overridePendingTransition(0,0);
-                    return  true;
+                    overridePendingTransition(0, 0);
+                    return true;
                 }
-                if(id==R.id.menu_notification){
+                if (id == R.id.menu_notification) {
                     startActivity(new Intent(getApplicationContext(), Notification.class));
-                    overridePendingTransition(0,0);
-                    return  true;
+                    overridePendingTransition(0, 0);
+                    return true;
                 }
-                if(id==R.id.menu_search){
+                if (id == R.id.menu_search) {
                     startActivity(new Intent(getApplicationContext(), Home.class));
-                    overridePendingTransition(0,0);
-                    return  true;
+                    overridePendingTransition(0, 0);
+                    return true;
                 }
-                if(id==R.id.menu_setting){
+                if (id == R.id.menu_setting) {
                     startActivity(new Intent(getApplicationContext(), Settings.class));
-                    overridePendingTransition(0,0);
-                    return  true;
+                    overridePendingTransition(0, 0);
+                    return true;
                 }
-                if(id==R.id.menu_profile){
+                if (id == R.id.menu_profile) {
                     startActivity(new Intent(getApplicationContext(), Profile.class));
-                    overridePendingTransition(0,0);
-                    return  true;
+                    overridePendingTransition(0, 0);
+                    return true;
                 }
                 return false;
             }
--- app/src/main/java/com/example/appsellbook/activities/ChangeSuccess.java
+++ app/src/main/java/com/example/appsellbook/activities/ChangeSuccess.java
@@ -1,6 +1,9 @@
 package com.example.appsellbook.activities;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.appsellbook.R;
 
 public class ChangeSuccess extends AppCompatActivity {
-
+    Button btn_done = findViewById(R.id.btn_done);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,11 @@ public class ChangeSuccess extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btn_done.setOnClickListener(v -> {
+            Intent intent = new Intent(ChangeSuccess.this,Home.class);
+            startActivity(intent);
+            overridePendingTransition(0, 0);
         });
     }
 }
